7bdc626e369dc7266940793d822a8fe430dce8de
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-07-26 01:02:50 -0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
 const buttons = document.querySelectorAll('.button')
 const screen = document.querySelector('.screen')
+const eraser = document.querySelector('#eraser')
 
-class Calculator {
-    constructor () {
+const calculator = {
+    screen: screen,
+    operation: '',
 
-    }
+    addDigit(digit) {
+        if (digit === '.' && this.screen.innerText.includes('.')) {
+            return console.log('Alredy have a dot')
+        }
+
+        this.screen.innerText += digit
+        
+        this.operation = this.screen.innerText
+    },
+
+    addOperator(operator) {
+        const operators = ['+', '-', '*', '/']
+
+        if (operators.includes(operator)) {
+            this.screen.innerText += operator
+        }
+
+        switch (operator) {
+            case '=':
+                this.resolveOperation()
+                break
+            case 'C':
+                this.clear()
+                break
+        }
+        
+    },
+
+    resolveOperation() {
+        this.screen.innerText = eval(this.operation)
+    },
+
+    clear() {
+        this.screen.innerText = ''
+    },
+
+    erase() {
+        this.screen.innerText = this.screen.innerText.slice(0, -1)
+    }   
 }
+
+buttons.forEach((i) => {
+    i.addEventListener('click', (e) => {
+        let value = e.target.innerText
+
+        if (isNaN(value) === false  || value === '.') {
+            calculator.addDigit(value)
+        } else {
+            calculator.addOperator(value)
+        }
+    })
+})
+
+eraser.addEventListener('click', () => {
+    calculator.erase()
+})
